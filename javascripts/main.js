eef7b3920b547a13da360f5b88a65bda98a30935
Deric Bunch <ddbunc2@gmail.com>
2015-08-11 11:39:22 -0500

    first commit

---
+++ javascripts/main.js
@@ -0,0 +1,23 @@
+requirejs.config({
+  baseUrl: './javascripts',
+  paths: {
+    'jquery': '../bower_components/jquery/dist/jquery.min',
+    'hbs': '../bower_components/require-handlebars-plugin/hbs',
+    'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
+    'firebase': '../bower_components/firebase/firebase',
+    'lodash': '../bower_components/lodash/lodash.min'
+  },
+  shim: {
+    'bootstrap': ['jquery'],
+    'firebase': {
+      exports: 'Firebase'
+    }
+  }
+});
+
+requirejs(["jquery", "lodash", "firebase", "hbs", "bootstrap"], function($, _, _firebase, Handlebars, bootstrap) {
+
+  var myFirebaseRef = new Firebase("https://glowing-torch-1013.firebaseio.com/");
+  myFirebaseRef.child("songs").on("value", function(snapshot) {
+
+});
