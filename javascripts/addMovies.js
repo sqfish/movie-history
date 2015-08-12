071889dbd1069b99ab7e6d1a788df9ff13bb3dba
Deric Bunch <ddbunc2@gmail.com>
2015-08-12 13:57:33 -0500

    functional posting to firebase and pulling from omdb

--- javascripts/addMovies.js
+++ javascripts/addMovies.js
@@ -1,41 +1,52 @@
-var addMovies = $('.movieName').val();
-var releaseDate = $('.releaseDate').val();
-var actors = $('.actors').val();
-var rating = $('#rating').val();
+requirejs.config({
+  baseUrl: './javascripts',
+  paths: {
+    'jquery': '../bower_components/jquery/dist/jquery.min',
+    'hbs': '../bower_components/require-handlebars-plugin/hbs',
+    'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min',
+    'firebase': '../bower_components/firebase/firebase',
+    'lodash': '../bower_components/lodash/lodash.min'
+  },
+  shim: {
+    'bootstrap': ['jquery'],
+    'firebase': {
+      exports: 'Firebase'
+    }
+  }
+});
 
- var watched;
-  $('.btn').click(function(){
-    watched = $(this).val();
-    console.log(watched);
+requirejs(["jquery", "lodash", "firebase", "hbs", "bootstrap", "searchMovieData"], 
+  function($, _, _firebase, Handlebars, bootstrap, searchMovie) {
+  
+  var myFirebaseRef = new Firebase("https://movie-database.firebaseio.com/");
+    myFirebaseRef.child("movie").on("value", function(snapshot) {
+    console.log(snapshot.val());
+
   });
 
+    var watched;
+    $('.btn').click(function(){
+      watched = Boolean($(this).val());
+      console.log(watched);
+    });
 
-$(document).on('click', '#submitMovie', function(){
-  
-})
 
-// $(document).on('click', '#submitSong', function() {  
-//   var song = $('.songName').val();
-//   console.log(song);
-//   var artist = $('.artistName').val();
-//   var album = $('.albumName').val();
-//   var year = $('.albumYear').val();
-//   console.log(genre);
-//   var addSong = {
-//     "songName": song,
-//     "artistName": artist,
-//     "albumName": album,
-//     "albumYear": year,
-//     "genre": genre
-//   };
-//   console.log(addSong);
-//   $(document).ready(function(){
-//     $.ajax({
-//       url: "https://glowing-torch-1013.firebaseio.com/songs.json",
-//       method: "POST",
-//       data: JSON.stringify(addSong)
-//     }).done(function(addSong) {
-//       console.log(addSong);
-//     });
-//   });
-// });
+  $(document).on('click', '#submitMovie', function(){
+    var movieName = $('.movieName').val();
+    var rating = parseInt($('#rating').val());
+      searchMovie.queryMovies(movieName, function(movies) {
+        var movieObj = movies;
+        movieObj.rating = rating;
+        movieObj.watched = watched;
+        console.log(movieObj);
+        console.log("data", movies);
+        $.ajax({
+          url: "https://movie-database.firebaseio.com/movie.json",
+          method: "POST",
+          data: JSON.stringify(movieObj)
+        }).done(function(movieObj) {
+          console.log(movieObj);
+        });
+      });
+  });
+});
